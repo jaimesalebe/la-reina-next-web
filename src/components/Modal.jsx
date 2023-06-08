520981b456b80a964cbb9b4e86dd6622836ccb08
Jaime Salebe <jaimesalebe@outlook.com>
2023-06-08 17:07:27 -0500

    fix image hover

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -26,8 +26,8 @@ function VideosSection({ videoID, singerImage }) {
 
     return (
         <>
-            <article className="w-96 hover:scale-105 transition-all duration-500">
-                <Image src={singerImage} alt="" height={600} width={500} onClick={handleOpen} className="cursor-pointer rounded-md h-full w-full " />
+            <article className="w-96">
+                <Image src={singerImage} alt="" height={600} width={500} onClick={handleOpen} className="cursor-pointer rounded-md h-full w-full hover:scale-105 transition-all duration-500 " />
                 <Modal handleClose={handleClose} isClose={isClose}>
                     {!isClose && <iframe src={`https://www.youtube.com/embed/${videoID}`} className="w-3/4 h-1/4 sm:w-1/2 sm:h-1/2 md:w-4/6 md:h-1/3 lg:w-1/2 lg:h-1/2"  />}
                 </Modal>
