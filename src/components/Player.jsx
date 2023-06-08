60e289b5206c1700744c110167f550a5d25f33a3
Jaime Salebe <jaimesalebe@outlook.com>
2023-06-08 12:13:19 -0500

    marquee fixed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -14,21 +14,23 @@ function Player() {
                 <audio ref={audioPlayer} src={frecuencia}></audio>
                 <div className={`container h-24 w-full flex justify-between items-center px-7 gap-10 ${error && "border border-red-700"}`}>
                     <div className="flex items-center flex-row-reverse gap-2">
-                        <select id='playerSelect' className={`text-darkblue-300 rounded-md bg-yellow font-semibold p-1 h-7`} onChange={handleFrecuencia}>
-                            <option value={""}>Ciudad</option>
-                            <option value="https://playerservices.streamtheworld.com/api/livestream-redirect/RNA_BARRANQUILLA.mp3">Barranquilla</option>
-                            <option value="https://playerservices.streamtheworld.com/api/livestream-redirect/RNA_CARTAGENA.mp3">Cartagena</option>
-                        </select>
+                        <div>
+                            {error
+                                && <marquee className='text-white text-center px-10'>
+                                    {`¡Algo ha salido mal! Escoja una ciudad valida o intente reproducir nuevamente.`}
+                                </marquee>
+
+                            }
+                            <select id='playerSelect' className={`text-darkblue-300 rounded-md bg-yellow font-semibold p-1 h-7`} onChange={handleFrecuencia}>
+                                <option value={""}>Ciudad</option>
+                                <option value="https://playerservices.streamtheworld.com/api/livestream-redirect/RNA_BARRANQUILLA.mp3">Barranquilla</option>
+                                <option value="https://playerservices.streamtheworld.com/api/livestream-redirect/RNA_CARTAGENA.mp3">Cartagena</option>
+                            </select>
+                        </div>
                         <span className="bg-black/50 rounded-md p-2">
                             <Logo />
                         </span>
                     </div>
-                    {error
-                        && <marquee className='text-white text-center px-10'>
-                            {`¡Algo ha salido mal! Escoja una ciudad valida o intente reproducir nuevamente.`}
-                        </marquee>
-
-                    }
                     <div className='flex justify-center items-center gap-10'>
                         <input type='range' className=" h-2 accent-yellow rounded-lg appearance-none cursor-grab bg-black/30 hidden sm:block" onChange={handleVolume} />
                         <button className={`bg-yellow hover:bg-yellow/70 p-4 rounded-full text-darkblue-300 font-semibold`} disabled={isLoading} onClick={togglePlayPause}> {isLoading ? <Waveform size={20} color='#212121' /> : isPlaying ? <FaPause /> : <FaPlay />} </button>
